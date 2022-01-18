90437e5783bca4ca3a29769a8f06cfbdba00f434
Xavier Accerani <xavier@MacBook-Air-de-Xavier.local>
2022-01-18 19:46:00 +0100

    make the api working

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 
@@ -7,6 +7,19 @@ import axios from "axios";
 const ProductListing = () => {
   const products = useSelector((state) => state);
   console.log("the product : ", products);
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("the error on axios : ", err);
+      });
+    console.log("the response : ", response);
+    console.log("the response data : ", response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  });
   return (
     <div className="ui grid container">
       <ProductComponent />
