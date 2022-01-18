214ec7450c0f6a00068963d861265c90d0db108f
Xavier Accerani <xavier@MacBook-Air-de-Xavier.local>
2022-01-18 19:36:05 +0100

    product listing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+import ProductComponent from "./containers/ProductComponent";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productI" exact element={<ProductDetail />} />
+          <Route> 404 not found </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
